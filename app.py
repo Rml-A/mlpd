94f69b9c706d77b3e7cfef6870bd64dad682ea0b
Ramil Akhmetov <ramiltat777@gmail.com>
2024-04-09 13:39:57 +0300

    create models(Task) and create tasks.db

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Task
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+# # Создание всех таблиц при первом запуске приложения
+# with app.app_context():
+#     db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/create', methods=['POST'])
+def create_task():
+    title = request.form['title']
+    description = request.form['description']
+    new_task = Task(title=title, description=description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete_task(id):
+    task_to_delete = Task.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
