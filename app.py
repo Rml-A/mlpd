6f9119b4cf0983cfe20e36c61f22ed60c42b320d
Ramil Akhmetov <ramiltat777@gmail.com>
2024-04-09 14:21:46 +0300

    add filter and sorting

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ db.init_app(app)
 # with app.app_context():
 #     db.create_all()
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
-    tasks = Task.query.all()
+    status_filter = request.args.get('status', '')
+    if status_filter:
+        tasks = Task.query.filter_by(status=status_filter).all()
+    else:
+        tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
+
 
 @app.route('/create', methods=['POST'])
 def create_task():
@@ -24,12 +30,33 @@ def create_task():
     db.session.commit()
     return redirect(url_for('index'))
 
+
 @app.route('/delete/<int:id>')
 def delete_task(id):
     task_to_delete = Task.query.get_or_404(id)
     db.session.delete(task_to_delete)
     db.session.commit()
     return redirect(url_for('index'))
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_task(id):
+    task = Task.query.get_or_404(id)
+    if request.method == 'POST':
+        task.title = request.form['title']
+        task.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit_task.html', task=task)
+
+
+@app.route('/view/<int:id>')
+def view_task(id):
+    task = Task.query.get_or_404(id)
+    return render_template('view_task.html', task=task)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
